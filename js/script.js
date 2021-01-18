95375b92dd2f164f7689fa3fd6d1af64918a18b4
HannaMazno <hanna.saldan1989@gmail.com>
2021-01-18 17:36:19 +0300

    floats were added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
-console.log(1);
+
+
+
+
+const telephonPrice = 400,
+      taxFee = 0.08,
+      bankBalanse = 2400,
+      acsecorizePrice = 50,
+      limit = 1500;
+ let amount = 0;     
+
+      function countTaxFee (amount) {
+        amount = amount*taxFee;
+        return amount;
+      }
+      
+      function goAround () {
+        return amount.toFixed(2);
+      }
+      
+      while(amount < limit) {
+        amount = amount + telephonPrice;
+        if (amout < )
+      }
+
+      let a = 12.577456746847;
+      console.log(a.toFixed(2));
